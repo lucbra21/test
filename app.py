7fd8e58fbb8395110b1942ef0623ec41139f32da
Lucas Bracamonte <ing.lucasbracamonte@gmail.com>
2024-11-14 12:44:05 -0300

    primer commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Título de la aplicación
+st.title("Hola desde Streamlit Clout")
+
+# Botón para mostrar el mensaje
+if st.button("Mostrar mensaje"):
+    st.write("Hola mundo")
